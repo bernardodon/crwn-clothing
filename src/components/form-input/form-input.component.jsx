a5ec8efdfe3ec38ff45e7d8bdc881bb0585aab78
Bernard Odon <bernardoodon@gmail.com>
2020-05-29 17:45:33 -0300

    finishing sign in handleSubmit method

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,7 +5,7 @@ const FormInput = ({handleChange, label, ...otherProps}) => {
     console.log(otherProps);
     return(
         <div className='group'>
-            <input className='form-input' onChange={handleChange}/>
+            <input className='form-input' onChange={handleChange} {...otherProps} />
             {
                 label ? 
                 (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
